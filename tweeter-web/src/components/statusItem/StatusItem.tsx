9fb361808be2fe9cdc118531cb0eb706fbbf7a69
leciojor <leciojor@byu.edu>
2026-02-03 01:07:13 -0700

    finished

--- tweeter-web/src/components/statusItem/StatusItem.tsx
+++ tweeter-web/src/components/statusItem/StatusItem.tsx
@@ -1,8 +1,7 @@
-import { Link, useNavigate } from "react-router-dom";
-import { Status, User, AuthToken, FakeData } from "tweeter-shared";
+import { Link } from "react-router-dom";
+import { Status } from "tweeter-shared";
 import Post from "../statusItem/Post";
-import { useMessageActions } from "../toaster/MessageHooks";
-import { useUserActions, useUserInfo } from "../userInfo/UserHooks";
+import { useUserNavigation } from "../userInfo/UserHooks";
 
 interface Props {
   item: Status;
@@ -11,43 +10,7 @@ interface Props {
 
 const StatusItem = (props: Props) => {
   const { item, featurePath } = props;
-  const { displayErrorMessage } = useMessageActions();
-  const { setDisplayedUser } = useUserActions();
-  const { displayedUser, authToken } = useUserInfo();
-
-  const navigate = useNavigate();
-
-  const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
-    event.preventDefault();
-
-    try {
-      const alias = extractAlias(event.target.toString());
-
-      const toUser = await getUser(authToken!, alias);
-
-      if (toUser) {
-        if (!toUser.equals(displayedUser!)) {
-          setDisplayedUser(toUser);
-          navigate(`${featurePath}/${toUser.alias}`);
-        }
-      }
-    } catch (error) {
-      displayErrorMessage(`Failed to get user because of exception: ${error}`);
-    }
-  };
-
-  const extractAlias = (value: string): string => {
-    const index = value.indexOf("@");
-    return value.substring(index);
-  };
-
-  const getUser = async (
-    authToken: AuthToken,
-    alias: string,
-  ): Promise<User | null> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.findUserByAlias(alias);
-  };
+  const { navigateToUser } = useUserNavigation(featurePath);
 
   return (
     <div className="col bg-light mx-0 px-0">
@@ -68,7 +31,7 @@ const StatusItem = (props: Props) => {
               </b>{" "}
               -{" "}
               <Link
-                to={`${props.featurePath}/${item.user.alias}`}
+                to={`${featurePath}/${item.user.alias}`}
                 onClick={navigateToUser}
               >
                 {item.user.alias}
--- tweeter-web/src/components/userInfo/UserHooks.ts
+++ tweeter-web/src/components/userInfo/UserHooks.ts
@@ -1,9 +1,58 @@
 import { useContext } from "react";
 import { UserInfoActionsContext, UserInfoContext } from "./UserInfoContexts";
+import { AuthToken, FakeData, User } from "tweeter-shared";
+import { useNavigate } from "react-router-dom";
+import { useMessageActions } from "../toaster/MessageHooks";
+
+interface UserNavigation {
+  navigateToUser: (event: React.MouseEvent) => Promise<void>;
+}
 
 export const useUserActions = () => {
   return useContext(UserInfoActionsContext);
 };
 export const useUserInfo = () => {
   return useContext(UserInfoContext);
+};
+
+export const useUserNavigation = (featurePath: string): UserNavigation => {
+  const { setDisplayedUser } = useUserActions();
+  const navigate = useNavigate();
+  const { displayedUser, authToken } = useUserInfo();
+  const extractAlias = (value: string): string => {
+    const index = value.indexOf("@");
+    return value.substring(index);
+  };
+  const { displayErrorMessage } = useMessageActions();
+
+  const getUser = async (
+    authToken: AuthToken,
+    alias: string,
+  ): Promise<User | null> => {
+    // TODO: Replace with the result of calling server
+    return FakeData.instance.findUserByAlias(alias);
+  };
+
+  return {
+    navigateToUser: async (event: React.MouseEvent): Promise<void> => {
+      event.preventDefault();
+
+      try {
+        const alias = extractAlias(event.target.toString());
+
+        const toUser = await getUser(authToken!, alias);
+
+        if (toUser) {
+          if (!toUser.equals(displayedUser!)) {
+            setDisplayedUser(toUser);
+            navigate(`${featurePath}/${toUser.alias}`);
+          }
+        }
+      } catch (error) {
+        displayErrorMessage(
+          `Failed to get user because of exception: ${error}`,
+        );
+      }
+    },
+  };
 };
